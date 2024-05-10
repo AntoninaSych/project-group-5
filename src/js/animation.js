cadef18df00bd53775592abdd6a2fb992a606344
Antonina Sych <antoninasych@gmail.com>
2024-05-10 16:06:05 +0100

    changed name animatio->proposal-box-animation

--- src/js/animation.js
+++ src/js/animation.js
@@ -1,6 +1,7 @@
-const element = document.querySelector('.proposal-box-animation');
+const element = document.querySelector('.proposal__box');
 const observer = new IntersectionObserver(entries => {
-  element.classList.toggle( 'proposal__box', entries[0].isIntersecting );
+  element.classList.toggle( 'animation-degree-9', entries[0].isIntersecting );
+  element.classList.toggle( 'animation-degree-11', entries[0].isIntersecting );
 });
 
 observer.observe( element );
