ff97e537f889f83988c9e7e094d8002ceff146c7
oleg-usatyi <oleg_usatyi@ukr.net>
2024-05-13 15:44:33 +0200

    prevent scroll if mobile menu open

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,28 +1,33 @@
 (() => {
-    const mobileMenu = document.querySelector('.js-menu-container')
-    const openMenuBtn = document.querySelector('.js-open-menu')
-    const closeMenuBtn = document.querySelector('.js-close-menu')
-    const menuLinks = document.querySelectorAll('.menu-link')
+  const mobileMenu = document.querySelector('.js-menu-container')
+  const openMenuBtn = document.querySelector('.js-open-menu')
+  const closeMenuBtn = document.querySelector('.js-close-menu')
+  const menuLinks = document.querySelectorAll('.menu-link')
 
-    const toggleMenu = () => {
-        const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true' || false
-        openMenuBtn.setAttribute('aria-expanded', !isMenuOpen)
-        mobileMenu.classList.toggle('is-open')
+  const toggleMenu = (isOpen = true) => () => {
+    openMenuBtn.setAttribute('aria-expanded', isOpen)
+
+    if (isOpen) {
+      mobileMenu.classList.add('is-open')
+      document.body.style.overflow = 'hidden'
+    } else {
+      mobileMenu.classList.remove('is-open')
+      document.body.style.overflow = 'auto'
     }
+  }
 
-    openMenuBtn.addEventListener('click', toggleMenu)
-    closeMenuBtn.addEventListener('click', toggleMenu)
-    menuLinks.forEach(menuLink => {
-      menuLink.addEventListener('click', toggleMenu)
-    })
+  openMenuBtn.addEventListener('click', toggleMenu())
+  closeMenuBtn.addEventListener('click', toggleMenu(false))
+  menuLinks.forEach(menuLink => {
+    menuLink.addEventListener('click', toggleMenu(false))
+  })
 
-    // Close the mobile menu on wider screens if the device orientation changes
-    window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
-        if (!e.matches) {
-          return
-        }
-        mobileMenu.classList.remove('is-open')
-        openMenuBtn.setAttribute('aria-expanded', false)
-    });
+  // Close the mobile menu on wider screens if the device orientation changes
+  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+    if (!e.matches) {
+      return
+    }
+    mobileMenu.classList.remove('is-open')
+    openMenuBtn.setAttribute('aria-expanded', false)
+  });
 })();
-
